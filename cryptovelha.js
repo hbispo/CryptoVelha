fa9d16cf9c91edc2b8473e446886d6cabee59d68
Henrique Bispo <rykbispos@gmail.com>
2017-05-10 16:33:55 -0300

    1.0
    Checagem de vitória

--- cryptovelha.js
+++ cryptovelha.js
@@ -8,7 +8,29 @@ lastoby = null;
 obx = 0;
 oby = 0;
 
+game = [3];
+game[1] = [3];
+game[2] = [3];
+game[3] = [3];
+
+game[1][1] = '';
+game[2][1] = '';
+game[3][1] = '';
+
+game[1][2] = '';
+game[2][2] = '';
+game[3][2] = '';
+
+game[1][3] = '';
+game[2][3] = '';
+game[3][3] = '';
+
+winner = '';
+
 function movement(cell, x, y, xx, yy) {
+    if(winner != '')
+        return;
+    
     if(choice == true){
         choose(x,y);
         return;
@@ -25,6 +47,10 @@ function movement(cell, x, y, xx, yy) {
     lastoby = oby;
     
     cell.appendChild(playerDot());
+    paintVictory(x,y,xx,yy)
+    
+    if(winner != '')
+        return;
     
     if(isFull(xx,yy)){
         choice = true;
@@ -44,6 +70,147 @@ function movement(cell, x, y, xx, yy) {
     changePlayer();
     
     paintBoard(obx,oby);
+}
+
+function paintVictory(x,y,xx,yy){
+    if(game[x][y] != '')
+        return;
+    
+    var cor = document.getElementById('x'+x+'y'+y+'xx'+xx+'yy'+yy).childNodes[0].style.color;
+    
+    var lin = false;
+    var lin1 = false;
+    var lin2 = false;
+    var lin3 = false;
+    
+    if(document.getElementById('x'+x+'y'+y+'xx1yy'+yy).innerHTML != '')
+        lin1 = document.getElementById('x'+x+'y'+y+'xx1yy'+yy).childNodes[0].style.color;
+    if(document.getElementById('x'+x+'y'+y+'xx2yy'+yy).innerHTML != '')
+        lin2 = document.getElementById('x'+x+'y'+y+'xx2yy'+yy).childNodes[0].style.color;
+    if(document.getElementById('x'+x+'y'+y+'xx3yy'+yy).innerHTML != '')
+        lin3 = document.getElementById('x'+x+'y'+y+'xx3yy'+yy).childNodes[0].style.color;
+    
+    if (cor == lin1 && lin1 == lin2 && lin2 == lin3)
+        lin = true;
+    
+    var col = false;
+    var col1 = false;
+    var col2 = false;
+    var col3 = false;
+    
+    if(document.getElementById('x'+x+'y'+y+'xx'+xx+'yy1').innerHTML != '')
+        col1 = document.getElementById('x'+x+'y'+y+'xx'+xx+'yy1').childNodes[0].style.color;
+    if(document.getElementById('x'+x+'y'+y+'xx'+xx+'yy2').innerHTML != '')
+        col2 = document.getElementById('x'+x+'y'+y+'xx'+xx+'yy2').childNodes[0].style.color;
+    if(document.getElementById('x'+x+'y'+y+'xx'+xx+'yy3').innerHTML != '')
+        col3 = document.getElementById('x'+x+'y'+y+'xx'+xx+'yy3').childNodes[0].style.color;
+    
+    if (cor == col1 && col1 == col2 && col2 == col3)
+        col = true;
+    
+    var dd = false;
+    if(xx == yy){
+        var dd1 = false;
+        var dd2 = false;
+        var dd3 = false;
+
+        if(document.getElementById('x'+x+'y'+y+'xx1yy1').innerHTML != '')
+            dd1 = document.getElementById('x'+x+'y'+y+'xx1yy1').childNodes[0].style.color;
+        if(document.getElementById('x'+x+'y'+y+'xx2yy2').innerHTML != '')
+            dd2 = document.getElementById('x'+x+'y'+y+'xx2yy2').childNodes[0].style.color;
+        if(document.getElementById('x'+x+'y'+y+'xx3yy3').innerHTML != '')
+            dd3 = document.getElementById('x'+x+'y'+y+'xx3yy3').childNodes[0].style.color;
+
+        if (cor == dd1 && dd1 == dd2 && dd2 == dd3)
+            dd = true;
+    }
+    
+    var da = false;
+    var dif = ((xx - yy) < 0 ? (xx - yy) * -1 : (xx - yy));
+    if(dif != 1){
+        var da1 = false;
+        var da2 = false;
+        var da3 = false;
+
+        if(document.getElementById('x'+x+'y'+y+'xx3yy1').innerHTML != '')
+            da1 = document.getElementById('x'+x+'y'+y+'xx3yy1').childNodes[0].style.color;
+        if(document.getElementById('x'+x+'y'+y+'xx2yy2').innerHTML != '')
+            da2 = document.getElementById('x'+x+'y'+y+'xx2yy2').childNodes[0].style.color;
+        if(document.getElementById('x'+x+'y'+y+'xx1yy3').innerHTML != '')
+            da3 = document.getElementById('x'+x+'y'+y+'xx1yy3').childNodes[0].style.color;
+
+        if (cor == da1 && da1 == da2 && da2 == da3)
+            da = true;
+    }
+    
+    if(lin){
+        paintLine(x,y,yy);
+        game[x][y] = cor;
+    }
+    if(col){
+        paintColumn(x,y,xx);
+        game[x][y] = cor;
+    }
+    if(dd){
+        paintDiag(x,y,'d');
+        game[x][y] = cor;
+    }
+    if(da){
+        paintDiag(x,y,'a');
+        game[x][y] = cor;
+    }
+    
+    if(game[x][y] != '')
+        paintEnd(x,y);
+}
+
+function paintEnd(x,y){
+    
+    var lin = false;
+    if(game[x][1] == game[x][2] && game[x][2] == game[x][3])
+        lin = true;
+    
+    var col = false;
+    if(game[1][y] == game[2][y] && game[2][y] == game[3][y])
+        col = true;
+    
+    var dd = false;
+    if(x==y && game[1][1] != '' && game[1][1] == game[2][2] && game[2][2] == game[3][3])
+        dd = true;
+    
+    var da = false;
+    if((x-y) != 1 && (x-y) != -1 && game[1][3] != '' && game[1][3] == game[2][2] && game[2][2] == game[3][1])
+        da = true;
+    
+    resetBoard();
+    
+    if(lin){
+        winner = game[x][y];
+        document.getElementById('x1y'+y).style.backgroundColor = 'gray';
+        document.getElementById('x2y'+y).style.backgroundColor = 'gray';
+        document.getElementById('x3y'+y).style.backgroundColor = 'gray';
+    }
+    if(col){
+        winner = game[x][y];
+        document.getElementById('x'+x+'y1').style.backgroundColor = 'gray';
+        document.getElementById('x'+x+'y2').style.backgroundColor = 'gray';
+        document.getElementById('x'+x+'y3').style.backgroundColor = 'gray';
+    }
+    if(dd){
+        winner = game[x][y];
+        document.getElementById('x1y1').style.backgroundColor = 'gray';
+        document.getElementById('x2y2').style.backgroundColor = 'gray';
+        document.getElementById('x3y3').style.backgroundColor = 'gray';
+    }
+    if(da){
+        winner = game[x][y];
+        document.getElementById('x1y3').style.backgroundColor = 'gray';
+        document.getElementById('x2y2').style.backgroundColor = 'gray';
+        document.getElementById('x3y1').style.backgroundColor = 'gray';
+    }
+    
+    if(winner != '')
+        document.getElementById('playerdiv').childNodes[0].textContent = "O jogo acabou! Vencedor: ";
 }
 
 function choose(x,y){
@@ -84,28 +251,34 @@ function changePlayer(){
     document.getElementById('playerdiv').childNodes[1].setAttribute("style", "color:#" + current);
 }
 
+function paintLine(x,y,yy){    
+    document.getElementById('x'+x+'y'+y+'xx1yy'+yy).style.backgroundColor = 'white';
+    document.getElementById('x'+x+'y'+y+'xx2yy'+yy).style.backgroundColor = 'white';
+    document.getElementById('x'+x+'y'+y+'xx3yy'+yy).style.backgroundColor = 'white';
+}
+
+function paintColumn(x,y,xx){
+    document.getElementById('x'+x+'y'+y+'xx'+xx+'yy1').style.backgroundColor = 'white';
+    document.getElementById('x'+x+'y'+y+'xx'+xx+'yy2').style.backgroundColor = 'white';
+    document.getElementById('x'+x+'y'+y+'xx'+xx+'yy3').style.backgroundColor = 'white';
+}
+
+function paintDiag(x,y,dir){
+    if(dir=='d'){
+        document.getElementById('x'+x+'y'+y+'xx1yy1').style.backgroundColor = 'white';
+        document.getElementById('x'+x+'y'+y+'xx2yy2').style.backgroundColor = 'white';
+        document.getElementById('x'+x+'y'+y+'xx3yy3').style.backgroundColor = 'white';
+    }
+    if(dir=='a'){
+        document.getElementById('x'+x+'y'+y+'xx3yy1').style.backgroundColor = 'white';
+        document.getElementById('x'+x+'y'+y+'xx2yy2').style.backgroundColor = 'white';
+        document.getElementById('x'+x+'y'+y+'xx1yy3').style.backgroundColor = 'white';
+    }
+}
+
 function paintArea(x,y){
     document.getElementById('x' + x + 'y' + y).style.backgroundColor = 'gray';
     document.getElementById('x' + x + 'y' + y).style.cursor = 'pointer';
-}
-
-function resetArea(x,y){
-    document.getElementById('x'+x+'y'+y).style.backgroundColor = '';
-    document.getElementById('x'+x+'y'+y).style.cursor = 'default';
-}
-
-function resetBoard(){
-    resetArea(1,1);
-    resetArea(2,1);
-    resetArea(3,1);
-    
-    resetArea(1,2);
-    resetArea(2,2);
-    resetArea(3,2);
-    
-    resetArea(1,3);
-    resetArea(2,3);
-    resetArea(3,3);
 }
 
 function paintBoard(x,y){
@@ -134,6 +307,25 @@ function paintBoard(x,y){
         if(!isFull(3,1))
             paintArea(3,3);
     }
+}
+
+function resetArea(x,y){
+    document.getElementById('x'+x+'y'+y).style.backgroundColor = '';
+    document.getElementById('x'+x+'y'+y).style.cursor = 'default';
+}
+
+function resetBoard(){
+    resetArea(1,1);
+    resetArea(2,1);
+    resetArea(3,1);
+    
+    resetArea(1,2);
+    resetArea(2,2);
+    resetArea(3,2);
+    
+    resetArea(1,3);
+    resetArea(2,3);
+    resetArea(3,3);
 }
 
 function desfazer() {
